25cfd0040d4e960710390947aa0cc59be1a6230a
Emirhan <emirhan.bda0@gmail.com>
2026-01-26 16:14:18 +0300

    .

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todo.routes");
 const errorHandler = require("./middlewares/errorHandler");
 const conn = require("./db");
+const userRouter = require("./routes/auth.routes");
 
 require("dotenv").config();
 conn();
@@ -11,6 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/api",userRouter);
 app.use("/todos", todoRoutes);
 
 app.use(errorHandler); // 3️⃣ ❗ EN SON
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,4 +15,29 @@ exports.register = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
-}
+};
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+
+        if (!user) { //User Boşsa
+            const err = new Error("Böyle Bir Kullanici Bulunamadi");
+            err.status = 404;
+            return next(err);
+
+        } else if (user.password != password) { //Şifreler DB`deki ile Eşleşmiyorsa
+            const err = new Error("Hatali Sifre");
+            err.status = 401;
+            return next(err);
+        }
+        res.status(200).json({
+            message: "Giris Basarili",
+            userName: user.username
+        })
+
+    } catch (err) {
+        next(err);
+    }
+};
